d23720a7d8acc1c8e7844ddcf211ff886c4fc9ff
Kemirdin <qemirdin@gmail.com>
2018-03-12 23:49:30 +0100

    html dom example

---
+++ Learn-JavaScript-HTML-DOM/HTML-DOM-Methods.js
@@ -0,0 +1,9 @@
+<!DOCTYPE html >
+    <html>
+    <body>
+        <h1> My Name is Divya </h1> <p id="demo"> </p>
+     <script>
+                document.getElementById("demo").innerHTML = "Hello Divya! Welcome..!!";
+    </script>
+            </body>
+            </html>
