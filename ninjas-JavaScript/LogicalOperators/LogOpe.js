32fc0128c55fdf0212908e0e01770c2f30be5ecb
Kemirdin <qemirdin@gmail.com>
2018-04-02 22:57:35 +0200

    JavaScript tutorial for beginners

---
+++ ninjas-JavaScript/LogicalOperators/LogOpe.js
@@ -0,0 +1,7 @@
+var myAge = 29;
+//if (myAge >= 18 && myAge <= 30)
+if (myAge < 18 || myAge > 30) {
+  document.write ('You csn come,you cool dude');
+} else {
+  document.write ('you aint coming!');
+}
