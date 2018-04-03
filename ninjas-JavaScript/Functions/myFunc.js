40da7d2090b27d55c06f26515639401fb486da22
Kemirdin <qemirdin@gmail.com>
2018-04-03 22:22:42 +0200

    JavaScript tutorial

---
+++ ninjas-JavaScript/Functions/myFunc.js
@@ -0,0 +1,8 @@
+function getAverage(a, b) {
+    var average = (a + b) / 2;
+    console.log(average);
+    return average;
+}
+
+var myResult = getAverage(7, 8);
+console.log("the average is " + myResult);
